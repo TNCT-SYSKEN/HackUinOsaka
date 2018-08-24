4f18868169837ae6aa02fadf93f835631907f40e
glatan <glatan.edu@gmail.com>
2018-08-24 18:53:17 +0900

    WebCameraの映像を見られるパネルを追加

---
+++ 千羽胡蝶/Assets/Scripts/Get_depth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Get_depth : MonoBehaviour {
+
+    private WebCamTexture webCamTexture;
+
+	// Use this for initialization
+	void Start () {
+
+        //Planeのレンダラー
+        Renderer renderer = GetComponent<Renderer>();
+        webCamTexture = new WebCamTexture();
+
+        //mainTextureにWebCamTextureを指定する
+        renderer.material.mainTexture = webCamTexture;
+        webCamTexture.Play();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
