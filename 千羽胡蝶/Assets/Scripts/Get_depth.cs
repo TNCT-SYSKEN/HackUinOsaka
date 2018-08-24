0925d2983c8c5563dbc25eff1a2470a297531607
glatan <glatan.edu@gmail.com>
2018-08-24 19:28:41 +0900

    帰る

--- 千羽胡蝶/Assets/Scripts/Get_depth.cs
+++ 千羽胡蝶/Assets/Scripts/Get_depth.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Get_depth : MonoBehaviour {
+    public int Width = 1920;
+    public int Height = 1080;
+    public int FPS = 30;
 
     private WebCamTexture webCamTexture;
+    private Color32[] color32s;
 
-	// Use this for initialization
 	void Start () {
 
         //Planeのレンダラー
@@ -19,8 +22,13 @@ public class Get_depth : MonoBehaviour {
 
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+        color32s = webCamTexture.GetPixels32();
+
+        Texture2D texture = new Texture2D(webCamTexture.width, webCamTexture.height);
+        GameObject.Find("Panel").GetComponent<Renderer>().material.mainTexture = texture;
+
+        texture.SetPixels32(color32s);
+        texture.Apply();
 	}
 }
