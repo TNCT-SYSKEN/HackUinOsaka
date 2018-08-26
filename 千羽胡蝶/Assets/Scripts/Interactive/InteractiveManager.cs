f4075a8183cc1847b403359c5b4ecdecfc694a6c
yato0421 <raizaraiza777@gmail.com>
2018-08-27 02:01:59 +0900

    ランダムな花火の生成

---
+++ 千羽胡蝶/Assets/Scripts/Interactive/InteractiveManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class InteractiveManager : MonoBehaviour {
+    //Triggerを宣言
+    public enum InteTrigger {wireworks, sample1};
+    public InteTrigger inte;
+    public GameObject webcamera;
+    public GameObject fire;
+    private WebCamTexture webCamTexture;
+
+    // Use this for initialization
+    void Start () {
+        webCamTexture = new WebCamTexture();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Triggerを更新
+        //string trigger = webcamera.GetComponent<WebcamCodeReader>().Read(webCamTexture);
+
+        switch (inte)
+        {
+            case InteTrigger.wireworks:
+                fire.GetComponent<Fireworks>().make_fireworks();
+                break;
+            case InteTrigger.sample1:
+                Debug.Log("sample1");
+                break;
+            default:
+                Debug.Log("End Interactive");
+                break;
+        }
+    }
+}
