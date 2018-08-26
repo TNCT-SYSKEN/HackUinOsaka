a2a03217dd4cc12ad59ed261b2b6e8969417bab9
glatan <glatan.edu@gmail.com>
2018-08-26 19:49:21 +0900

    名前を修正

---
+++ 千羽胡蝶/Assets/Scripts/rotate_fire.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate_fire : MonoBehaviour {
+    public int y_rotate;
+
+	void Update () {
+        //打ちあがる花火の軸を回転させる
+        transform.Rotate(new Vector3(0, y_rotate, 0) * Time.deltaTime, Space.World);
+	}
+}
