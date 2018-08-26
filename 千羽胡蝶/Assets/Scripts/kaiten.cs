0b4071d6109a2315ea87af85be16c7365aef7ce4
glatan <glatan.edu@gmail.com>
2018-08-26 19:23:27 +0900

    add_fireworks_asset

---
+++ 千羽胡蝶/Assets/Scripts/kaiten.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kaiten : MonoBehaviour {
+    public int a;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, a, 0) * Time.deltaTime, Space.World);
+	}
+}
