a127537bbecb0d7686b2d6f28302c55e7b722e07
terfno <sueda.takahito@gmail.com>
2018-08-30 04:14:43 +0900

    金魚ムーンウォークｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗ

---
+++ 手妻/Assets/scripts/goldfish.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goldfish : MonoBehaviour {
+    public float max_x_grid = 9.78f; //波右の限界値
+    public float min_x_grid = -10.2f; //波左の限界値
+    public float max_y_grid = -3.83f; //波上の限界値
+    public float min_y_grid = -5.02f; //波下の限界値
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 now_pos = this.gameObject.transform.position;
+        this.gameObject.transform.position = new Vector3(now_pos.x + 0.05f, now_pos.y, now_pos.z);
+	}
+}
