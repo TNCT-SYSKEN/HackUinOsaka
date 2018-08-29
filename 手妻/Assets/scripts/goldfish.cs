852f507620debbf6f70b303f8fa778542ed3a7b2
terfno <sueda.takahito@gmail.com>
2018-08-30 04:30:59 +0900

    謎Errorで死亡

--- 手妻/Assets/scripts/goldfish.cs
+++ 手妻/Assets/scripts/goldfish.cs
@@ -8,6 +8,10 @@ public class goldfish : MonoBehaviour {
     public float max_y_grid = -3.83f; //波上の限界値
     public float min_y_grid = -5.02f; //波下の限界値
 
+    public float timeOut = 3.5f;
+    public char gold_dir = "r";
+    public float timeElapsed;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -15,7 +19,25 @@ public class goldfish : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 now_pos = this.gameObject.transform.position;
-        this.gameObject.transform.position = new Vector3(now_pos.x + 0.05f, now_pos.y, now_pos.z);
+        timeElapsed += Time.deltaTime;
+        if(timeElapsed < timeOut){
+            if (gold_dir === "r")
+            {
+                Vector3 now_pos = this.gameObject.transform.position;
+                this.gameObject.transform.position = new Vector3(now_pos.x + 0.05f, now_pos.y, now_pos.z);
+            }
+            else
+            {
+                Vector3 now_pos = this.gameObject.transform.position;
+                this.gameObject.transform.position = new Vector3(now_pos.x - 0.05f, now_pos.y, now_pos.z);
+            }
+        }else{
+            if(gold_dir === "r"){
+                gold_dir = "l";
+            }else{
+                gold_dir = "r";
+            }
+            timeElapsed = 0.0f;
+        }
 	}
 }
