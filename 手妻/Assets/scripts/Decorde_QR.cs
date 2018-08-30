7573a4ec1bbe829c9eb9fccb5e5f6c1da5a1cffd
glatan <glatan.edu@gmail.com>
2018-08-30 13:14:32 +0900

    some files moved from 千羽胡蝶

---
+++ 手妻/Assets/scripts/Decorde_QR.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using ZXing;
+
+public class WebcamCodeReader {
+    private Color32[] flame_data = null;
+    private BarcodeReader qr_reader = new BarcodeReader();
+
+    public string Read(WebCamTexture webCamTexture) {
+        int width = webCamTexture.width;
+        int height = webCamTexture.height;
+
+        if (flame_data == null) {
+            flame_data = new Color32[width * height];
+        }
+
+        webCamTexture.GetPixels32(flame_data);
+
+        Result result = qr_reader.Decode(flame_data, width, height);
+
+        if (result != null) {
+            return result.Text;
+        }
+
+        return null;
+    }
+}
